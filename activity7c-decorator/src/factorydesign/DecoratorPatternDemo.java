e391af97098a222eb5407f2398c295051d62d2f0
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2020-11-10 01:51:33 -0700

    Add files via upload

--- activity7c-decorator/src/factorydesign/DecoratorPatternDemo.java
+++ activity7c-decorator/src/factorydesign/DecoratorPatternDemo.java
@@ -2,27 +2,29 @@ package factorydesign;
 
 public class DecoratorPatternDemo {
 	public static void main(String[] args) {
-		Pizza topping = new ToppingDecorator(new CheesePizza());
+		Pizza cheeseTopping = new ToppingDecorator(new CheesePizza());
 		
 		Pizza cheese = new CheesePizza();
 		cheese.prepare();
-		topping.prepare();
+		cheeseTopping.prepare();
 		cheese.bake();
 		cheese.slice();
 		cheese.pack();
 		System.out.println("\n");
 		
+		Pizza pepperoniTopping = new ToppingDecorator(new PepperoniPizza());
 		Pizza pepperoni = new PepperoniPizza();
 		pepperoni.prepare();
-		topping.prepare();
+		pepperoniTopping.prepare();
 		pepperoni.bake();
 		pepperoni.slice();
 		pepperoni.pack();
 		System.out.println("\n");
 		
+		Pizza hawaiianTopping = new ToppingDecorator(new HawaiianPizza());
 		Pizza hawaiian = new HawaiianPizza();
 		hawaiian.prepare();
-		topping.prepare();
+		hawaiianTopping.prepare();
 		hawaiian.bake();
 		hawaiian.slice();
 		hawaiian.pack();
